8b38eb412c34206ead4b129ad59e8db314d44a9b
Luxx Labs <13350855+luxxyarns@users.noreply.github.com>
2021-08-12 10:38:36 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -1,7 +1,20 @@
+// swift-tools-version:5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
 	name: "FolioReaderKit",
+	    platforms: [.iOS(.v14)],
+
+	
+	 products: [
+        .library(
+            name: "FolioReaderKit",
+            targets: ["FolioReaderKit" ]),
+ 
+    ],
+	
 	dependencies: [
 		.Package(url: "https://github.com/ZipArchive/ZipArchive.git", majorVersion: 2, minor: 1),
 		.Package(url: "https://github.com/cxa/MenuItemKit.git", majorVersion: 3, minor: 0),
@@ -11,4 +24,25 @@ let package = Package(
 		.Package(url: "https://github.com/fantim/JSQWebViewController.git", majorVersion: 6, minor: 1),
 		.Package(url: "https://github.com/realm/realm-cocoa.git", majorVersion: 3, minor: 1),
 	]
+	
+	
+	,
+	 targets: [
+        .target(
+            name: "FolioReaderKit",
+            dependencies: [
+                .product(name: "ZipArchive", package: "ZipArchive") , 
+		       .product(name: "MenuItemKit", package: "MenuItemKit") , 
+		       .product(name: "ZFDragableModalTransition", package: "ZFDragableModalTransition") , 
+		       .product(name: "AEXML", package: "AEXML") , 
+		       .product(name: "FontBlaster", package: "FontBlaster") , 
+		       .product(name: "JSQWebViewController", package: "ZipArcJSQWebViewControllerhive") , 
+		       .product(name: "realm-cocoa", package: "realm-cocoa") , 
+		    
+            ], 
+        ),
+ 
+    ],
+
+    swiftLanguageVersions: [ .v5]
 )
